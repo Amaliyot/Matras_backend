4c22ebbc8833b95dc0d566c1d38be4f09c4d8610
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-04 12:51:05 +0500

    Create server

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+// const routes = require("./routes/routes");
+// const postgres = require("./modules/postgres/postgres");
+// const customErrorMiddleware = require("./middlewares/customErrorMiddleware");
+
+async function server(port) {
+	try {
+		app.listen(process.env.PORT || 3000, () =>
+			console.log(`SERVER READY ${port || 3000}`)
+		);
+
+		// const db = await postgres();
+
+		app.use(async (req, res, next) => {
+            req.db = await db;
+            next();
+        });
+
+		// app.use(customErrorMiddleware);
+
+		app.use(express.json());
+		app.use(express.urlencoded({ extended: true }));
+
+	} catch (error) {
+		console.log("SERVER ERROR", error);
+	} finally {
+		// routes(app);
+	}
+}
+
+module.exports = server
