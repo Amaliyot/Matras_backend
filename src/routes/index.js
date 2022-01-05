c771e56785a75acf154c978a19857a80d76d3261
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-05 23:56:18 +0500

    add admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const Router = require("express").Router();
 
+const AdminRouter = require("./AdminRoute");
 const HomeRouter = require("./HomeRoute");
 
 Router.use("/", HomeRouter)
+Router.use("/admin", AdminRouter)
 
 module.exports = Router
