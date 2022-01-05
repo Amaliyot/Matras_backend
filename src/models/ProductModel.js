bfe9f12501e624e3dc4a54eb211c3d8057764857
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-05 12:20:16 +0500

    Update product model

---
+++ src/models/ProductModel.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, Sequelize) => {
+    sequelize.define("products", {
+        product_id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4(),
+            primaryKey: true,
+            allowNull: false
+        },
+        product_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        product_price: {
+            type: Sequelize.BIGINT,
+            allowNull: false
+        },
+        product_weight: {
+            type: Sequelize.BIGINT,
+            allowNull: false
+        },
+        product_size: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        product_warranty_duration: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        product_capacity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        product_hasDiscount: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+        product_discount_price: {
+            type: Sequelize.BIGINT,
+            allowNull: false
+        }
+    })
+}
