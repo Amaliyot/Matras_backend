0325d8356dddbc26518af2123a6822cd28e37323
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-05 12:23:09 +0500

    Update technology model

---
+++ src/models/TechnologyModel.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+    sequelize.define("categories", {
+        category_id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4(),
+            primaryKey: true,
+            allowNull: false
+        },
+        category_name: {
+            type: Sequelize.STRING(64),
+            allowNull: false
+        },
+        category_description: {
+            type: Sequelize.STRING(1024),
+            allowNull: false
+        },
+        category_photo: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        category_video: {
+            type: Sequelize.STRING,
+            allowNull: true
+        }
+    })
+}
