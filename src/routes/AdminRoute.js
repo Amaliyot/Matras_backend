5b70166122c7a2b5d76cbfbf27e346cc7321c560
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-06 00:36:30 +0500

    update

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,5 +1,8 @@
+const { AdminCreateProductPostController, AdminCreateCategoryPostController } = require("../controllers/AdminController");
+
 const AdminRouter = require("express").Router();
 
-AdminRouter.get("/")
+AdminRouter.post("/categories/new", AdminCreateCategoryPostController)
+AdminRouter.post("/products/new", AdminCreateProductPostController)
 
 module.exports = AdminRouter
