a0b8b29aa564c9f0f666b4c52119316d5f46085b
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-06 00:25:34 +0500

    create product controller

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,4 +1,4 @@
-const { CategoryValidation } = require("../modules/validations")
+const { CategoryValidation, ProductValidation } = require("../modules/validations")
 
 module.exports = class AdminController{
     static async AdminCreateCategoryPostController(req, res, next){
@@ -15,6 +15,33 @@ module.exports = class AdminController{
             res.status(201).json({
                 ok: true,
                 message: "Category created succesfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async AdminCreateProductPostController(req, res, next){
+        try {
+            const data = await ProductValidation(req.body, res.error)
+
+            const new_product = await req.db.categories.create({
+                product_name: data.name,
+                product_price: data.price,
+                product_weight: data.weight,
+                product_size: data.size,
+                product_warranty_duration: data.warranty,
+                product_capacity: data.capacity,
+                product_isNew: data.isNew,
+                product_isActive: data.isActive,
+                product_hasDiscount: data.hasDiscount,
+                product_discount_price: data.discountPrice,
+            })
+
+            if (!new_product) throw new res.error(500, "Something went wrong while creating category!")
+
+            res.status(201).json({
+                ok: true,
+                message: "Product created succesfully"
             })
         } catch (error) {
             next(error)
