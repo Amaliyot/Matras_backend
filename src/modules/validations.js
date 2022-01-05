7e3d5ab2d19c4788a71c052981e2358cf940244c
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-05 23:51:27 +0500

    create category validation

--- src/modules/validations.js
+++ src/modules/validations.js
@@ -7,4 +7,11 @@ module.exports = class Validations {
             password: JOI.string().required().min(5).error(new CustomError(400, "Password is invalid"))
         }).validateAsync(data)
     }
+
+    static async CategoryValidation(data, CustomError){
+        return await JOI.object({
+            name: JOI.string().required().max(32).error(new CustomError(400, "Category name is invalid")),
+            status: JOI.boolean().required().error(new CustomError(400, "Status is invalid"))
+        })
+    }
 }
