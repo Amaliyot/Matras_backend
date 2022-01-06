13e7716f14b7c2cdea52285f6f657b4aa62d34fc
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-06 16:31:37 +0500

    change controllers file structure

---
+++ src/controllers/Admin/CategoryController.js
@@ -0,0 +1,21 @@
+module.exports = class CategoryController{
+    static async CreateCategoryPostController(req, res, next){
+        try {
+            const data = await CategoryValidation(req.body, res.error)
+
+            const new_category = await req.db.categories.create({
+                category_name: data.name,
+                category_status: data.status
+            })
+
+            if (!new_category) throw new res.error(500, "Something went wrong while creating category!")
+
+            res.status(201).json({
+                ok: true,
+                message: "Category created succesfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+}
