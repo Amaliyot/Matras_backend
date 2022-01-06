812ff73a8502b3db227de589efb97f708753cd7d
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-06 16:56:52 +0500

    update product controller

--- src/controllers/Admin/ProductController.js
+++ src/controllers/Admin/ProductController.js
@@ -3,6 +3,41 @@ module.exports = class ProductController{
         try {
             const data = await ProductValidation(req.body, res.error)
 
+            const category = await req.db.categories.findOne({
+                where: {
+                    category_id: data.category
+                }
+            })
+
+            if(!category) throw new res.error(400, "Category not found")
+
+            const allowedTypeForFile = [
+				".png",
+				".jpg",
+				".jpeg",
+			];
+
+            let files = req.files?.files
+            
+            if (!Array.isArray(files) && files) {
+				files = [req.files?.files];
+			}
+
+            if (!files) throw new res.error(400, "Files not found");
+			if (files?.length > 4) throw new res.error(400, "Too many files");
+
+            files.map(file => {
+                if (
+                    allowedTypeForFile.includes(getExtension(file.name))
+                ){
+                    throw new res.error(400, `${getExtension(file.name)} files are not allowed`)
+                }else if (
+                    file.size > 100 * 1024000
+                ){
+                    throw new res.error(400, `Files are too large`)
+                }
+            })
+
             const new_product = await req.db.categories.create({
                 photos: data.photos,
                 product_name: data.name,
@@ -15,6 +50,7 @@ module.exports = class ProductController{
                 product_isActive: data.isActive,
                 product_hasDiscount: data.hasDiscount,
                 product_discount_price: data.discountPrice,
+                category_id: category.category_id,
             })
 
             if (!new_product) throw new res.error(500, "Something went wrong while creating category!")
@@ -27,4 +63,9 @@ module.exports = class ProductController{
             next(error)
         }
     }
+}
+
+function getExtension(filename) {
+	var i = filename.lastIndexOf(".");
+	return i < 0 ? "" : filename.substr(i);
 }
