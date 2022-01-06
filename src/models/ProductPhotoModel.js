6414da1e023a3b69c70629d932ca409bdaca853b
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-07 01:25:48 +0500

    add photo model

---
+++ src/models/ProductPhotoModel.js
@@ -0,0 +1,18 @@
+module.exports = async (sequelize, Sequelize) => {
+    return await sequelize.define("photos", {
+        photo_id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4(),
+            primaryKey: true,
+            allowNull: false
+        },
+        photo_name: {
+            type: Sequelize.STRING(64),
+            allowNull: false
+        },
+        photo_ext: {
+            type: Sequelize.STRING(5),
+            allowNull: false
+        }
+    })
+}
