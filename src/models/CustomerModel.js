bf24e24171d110e454b4af784db42ae8f514a750
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-06 11:36:08 +0500

    fix

--- src/models/CustomerModel.js
+++ src/models/CustomerModel.js
@@ -7,7 +7,7 @@ module.exports = async (sequelize, Sequelize) => {
             allowNull: false
         },
         customer_serial_id: {
-            type: DataTypes.INTEGER,
+            type: Sequelize.INTEGER,
             unique: true,
             primaryKey: true,
             autoIncrement: true
