835388e63e59c296e42cf9891f7f5adac3fa2274
Dilmurod-WebDev <dilmurodnabiev00@gmail.com>
2022-01-07 10:41:41 +0500

    add DeleteProductPostController

--- src/controllers/Admin/ProductController.js
+++ src/controllers/Admin/ProductController.js
@@ -1,9 +1,38 @@
+const { ProductValidation } = require("../../modules/validations")
+
 module.exports = class ProductController{
+    static async DeleteProductPostController(req, res, next){
+        try {
+            const data = await ProductValidation(req.body, res.error)
+
+            const isProduct = await req.db.products.findOne({
+                product_id: req.params.id
+            })
+
+            if(!isProduct) throw new res.error(400, "Product not found")
+
+            const product = await req.db.products.update({
+                where: {
+                    product_id: req.params.id
+                }
+            })
+
+            if(!product) throw new res.error(500, "Something went wrong while deleting category!")
+
+            res.status(201).json({
+                ok: true,
+                message: "Product deleted succesfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+//-------------------------------------------------------------------------
     static async CreateProductPostController(req, res, next){
         try {
             const data = await ProductValidation(req.body, res.error)
 
-            const new_product = await req.db.categories.create({
+            const new_product = await req.db.categories.create({ 
                 photos: data.photos,
                 product_name: data.name,
                 product_price: data.price,
