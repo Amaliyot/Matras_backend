c36bb7932d63bb74e0f57a2d6ba33861f30abb42
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-10 17:11:31 +0500

    add address to route

--- src/controllers/Admin/FilesController.js
+++ src/controllers/Admin/FilesController.js
@@ -37,6 +37,23 @@ module.exports = class FilesController{
         }
     }
 
+    static async AdressPhotosGetController(req, res, next){
+        try {
+            const photos = await req.db.adress_photos.findAll()
+
+            if(!photos) throw new res.error(400, "Files not found")
+
+            res.status(200).json({
+                ok: true,
+                data: {
+                    photos
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
     static async DeleteProductPhotoPostController(req, res, next){
         try {
             const photo = await req.db.photos.findOne({
@@ -79,6 +96,32 @@ module.exports = class FilesController{
                 }
             })
             fs.unlink(path.join(__dirname, '..', '..', 'public', 'files', 'technologyPhotos', `${photo.photo_name + photo.photo_ext}`))
+            
+            res.status(200).json({
+                ok: true,
+                message: "File deleted successfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async DeleteAdressPhotoPostController(req, res, next){
+        try {
+            const photo = await req.db.adress_photos.findOne({
+                where: {
+                    photo_id: req.params.id
+                }
+            })
+
+            if(!photo) throw new res.error(400, "File not found")
+
+            await req.db.adress_photos.destroy({
+                where: {
+                    photo_id: photo.photo_id
+                }
+            })
+            fs.unlink(path.join(__dirname, '..', '..', 'public', 'files', 'adressPhotos', `${photo.photo_name + photo.photo_ext}`))
             
             res.status(200).json({
                 ok: true,
--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,5 +1,6 @@
+const { AdressesGetController } = require("../controllers/Admin/AddressController");
 const { CategoriesGetController } = require("../controllers/Admin/CategoryController");
-const { ProductPhotosGetController, TechnologyPhotosGetController } = require("../controllers/Admin/FilesController");
+const { ProductPhotosGetController, TechnologyPhotosGetController, AdressPhotosGetController } = require("../controllers/Admin/FilesController");
 const { ProductsGetController } = require("../controllers/Admin/ProductController");
 const { TechnologiesGetController } = require("../controllers/Admin/TechnologyController");
 const { HomeRouteGetController } = require("../controllers/Home/HomeController");
@@ -9,8 +10,10 @@ const HomeRouter = require("express").Router();
 HomeRouter.get("/", HomeRouteGetController)
 HomeRouter.get("/products/files", ProductPhotosGetController)
 HomeRouter.get("/technologies/files", TechnologyPhotosGetController)
+HomeRouter.get("/addresses/files", AdressPhotosGetController)
 HomeRouter.get("/technologies", TechnologiesGetController)
 HomeRouter.get("/products", ProductsGetController)
 HomeRouter.get("/categories", CategoriesGetController)
+HomeRouter.get("/addresses", AdressesGetController)
 
 module.exports = HomeRouter
