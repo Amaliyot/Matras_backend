7695c698fcd87dd0950ca7361a2eb85874468ba0
Dilmurod-WebDev <dilmurodnabiev00@gmail.com>
2022-01-10 12:30:11 +0500

    Create ClientPhoneController

--- src/controllers/Admin/ClientPhoneController.js
+++ src/controllers/Admin/ClientPhoneController.js
@@ -16,5 +16,45 @@ module.exports = class ClientPhoneController{
         })
     }
 
-    
+    static async DeleteClientPhonePostController(req, res, next){
+        let message = "Client phone deleted successfully."
+        const id = req.params.id
+        const isClientPhone = await req.db.client_phone.findOne({
+            where: {
+                client_phone_id: id
+            }
+        })
+
+        if(!isClientPhone) throw new res.error(400,"Client phone is not found")
+
+        const clientPhone = await req.db.client_phone.destroy({
+            where: {
+                client_phone_id: id
+            }
+        })
+
+        if(!clientPhone) throw new res.error(500, "Something went wrong while creating client phone!")
+
+        res.status(200).json({
+            ok: true,
+            message: "Client phone deleted successfully"
+        })
+    }
+
+    static async ClientPhonesGetController(req, res, next){
+        try {
+            const client_phones = await req.db.client_phone.findAll()
+
+            if(!client_phones) throw new res.error(400, "Could not get client phones")
+
+            res.status(200).json({
+                ok: true,
+                data: {
+                    client_phones
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
 }
