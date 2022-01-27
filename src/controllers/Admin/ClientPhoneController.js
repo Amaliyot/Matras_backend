a9368ca5e141448b7c7c0e8a01c1b33c3a377830
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-27 11:51:45 +0500

    update

--- src/controllers/Admin/ClientPhoneController.js
+++ src/controllers/Admin/ClientPhoneController.js
@@ -7,14 +7,46 @@ module.exports = class ClientPhoneController{
 
             const new_client_phone = await req.db.client_phone.create({
                 client_phone_number: data.phone,
-                client_phone_status: data.status
             })
 
             if(!new_client_phone) throw new res.error(500, "Something went wrong while creating client phone!")
 
             res.status(201).json({
                 ok: true,
-                message: "Client phone created succesfully"
+                message: "Client phone created succesfully",
+                data: {
+                    client: new_client_phone
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async UpdateClientPhonePostController(req, res, next){
+        try {
+            const {
+                params
+            } = req
+            const data = await ClientPhoneValidation(req.body, res.error)
+
+            const client_phone = await req.db.client_phone.update({
+                client_phone_status: data.status,
+            },{
+                where: {
+                    client_phone_id: params.id
+                },
+                returning: true
+            })
+
+            if(!client_phone) throw new res.error(500, "Something went wrong while updating client phone!")
+
+            res.status(200).json({
+                ok: true,
+                message: "Client phone updated succesfully",
+                data: {
+                    client: client_phone
+                }
             })
         } catch (error) {
             next(error)
