42d0bc56e7260d6b08fee61dc97ff6ee2b167555
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-01-27 11:51:49 +0500

    update

--- src/controllers/Admin/OrderController.js
+++ src/controllers/Admin/OrderController.js
@@ -22,7 +22,46 @@ module.exports = class OrderController{
 
             res.status(201).json({
                 ok: true,
-                message: "Order created successfully"
+                message: "Order created successfully",
+                data: {
+                    order: new_order
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async OrderUpdatePutController(req, res, next){
+        try {
+            const { body, params } = req
+            const data = await OrderValidation(body, res.error);
+
+            const product = await req.db.products.findOne({
+                where: {
+                    product_id: data.product_id
+                }
+            })
+
+            if(!product) throw new res.error(400, "Product not found")
+
+            const order = await req.db.orders.update({
+                order_status: data.status
+            }, {
+                where: {
+                    order_id: params.id
+                },
+                returning: true
+            })
+
+            if(!order) throw new res.error(400, "Something went wrong")
+
+            res.status(200).json({
+                ok: true,
+                message: "Order updated successfully",
+                data: {
+                    order
+                }
             })
         } catch (error) {
             next(error)
